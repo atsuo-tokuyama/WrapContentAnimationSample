40ac16156d7b7b3f728f2102702cf7677317c61a
Atsuo Tokuyama <atsuo.tokuyama@westunitis.co.jp>
2022-11-08 15:30:54 +0900

    拡大、縮小アニメーションを実装

--- app/src/main/java/yama/toku/wrapcontentanimationsample/MainActivity.kt
+++ app/src/main/java/yama/toku/wrapcontentanimationsample/MainActivity.kt
@@ -1,7 +1,13 @@
 package yama.toku.wrapcontentanimationsample
 
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
 import yama.toku.wrapcontentanimationsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,16 +20,57 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.toggleButton.setOnClickListener {
-
+            if (binding.textView.visibility == View.VISIBLE) {
+                collapse()
+            } else {
+                expand()
+            }
         }
     }
 
+    // 縮小
     private fun collapse() {
-        
-
+        val actualHeight = binding.textView.height
+        ValueAnimator.ofInt(actualHeight, 1).apply {
+            duration = 200
+            addUpdateListener { animator ->
+                val newValue = animator.animatedValue as Int
+                val layoutParams = binding.textView.layoutParams.apply {
+                    height = newValue
+                }
+                binding.textView.layoutParams = layoutParams
+            }
+            doOnEnd {
+                binding.textView.visibility = View.INVISIBLE
+            }
+            start()
+        }
     }
 
+    // 拡大
     private fun expand() {
-
+        // [POINT]WRAP_CONTENTを指定した時の高さを計算します。
+        binding.textView.measure(WRAP_CONTENT, WRAP_CONTENT)
+        val expectedHeight = binding.textView.measuredHeight
+        ValueAnimator.ofInt(1, expectedHeight).apply {
+            duration = 200
+            addUpdateListener { animator ->
+                val newValue = animator.animatedValue as Int
+                val layoutParams = binding.textView.layoutParams.apply {
+                    height = newValue
+                }
+                binding.textView.layoutParams = layoutParams
+            }
+            doOnStart {
+                binding.textView.visibility = View.VISIBLE
+            }
+            doOnEnd {
+                val layoutParams = binding.textView.layoutParams.apply {
+                    height = WRAP_CONTENT
+                }
+                binding.textView.layoutParams = layoutParams
+            }
+            start()
+        }
     }
 }
